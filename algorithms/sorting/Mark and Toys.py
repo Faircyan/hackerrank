34bc497e6f6b6a7ac2db66037cb0928b9514cefe
Faircyan <33095502+Faircyan@users.noreply.github.com>
2018-10-02 18:31:12 +0300

    Marks And Toys Sort
    Marks And Toys Sort

---
+++ algorithms/sorting/Mark and Toys.py
@@ -0,0 +1,48 @@
+def mergeSort (alist):
+    if len(alist) > 1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+        i = 0
+        j = 0
+        k = 0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k] = lefthalf[i]
+                i += 1
+            else:
+                alist[k] = righthalf[j]
+                j += 1
+            k += 1
+
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i += 1
+            k += 1
+
+        while j < len(righthalf):
+            alist[k] = righthalf[j]
+            j += 1
+            k += 1
+
+def maximumToys():
+
+    budget = input().split()[1]
+    prices = list(map(int, input().rstrip().split()))
+    return budget, prices
+
+
+budget, list = maximumToys()
+
+mergeSort(list)
+budget = int(budget)
+counter = 0
+
+for i in list:
+    if(budget >= i):
+        budget -= i
+        counter += 1
+print(counter)
